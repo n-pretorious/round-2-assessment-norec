f94bb7d18a11224f3e072e38ab148b3a4736f86f
n-pretorious <pretoriousndungu@gmail.com>
2020-02-21 12:43:52 +0300

    chaned key for mongoURI

--- server/src/config/keys.ts
+++ server/src/config/keys.ts
@@ -1,4 +1,4 @@
 export default {
-    mongoURI: 'mongodb+srv://pretoriousN:test2020@cluster0-kux14.mongodb.net/test?retryWrites=true&w=majority',
+    mongoURI: 'paste your own mongodb connection string',
     JWT_secret_key: 'this is a secret'
 }
